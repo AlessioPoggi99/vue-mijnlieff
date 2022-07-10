090ac887f240e1a695a672f33998e3cadb4d0954
Alessio Poggi <alessio.poggi01@universitadipavia.it>
2022-07-10 23:04:35 +0200

    implemented 2 player rooms and online game

--- src/views/OnlineGame.vue
+++ src/views/OnlineGame.vue
@@ -3,6 +3,47 @@ import { ref, watchEffect } from 'vue'
 import Tile from '../components/Tile.vue';
 import Board from '../components/Board.vue';
 import { tilesRotation } from '../store/store.js'
+import io from "socket.io-client";
+
+/*** FOR TESTING ***/
+const isOnlineGame = true
+
+
+/*** HANDLE CONNECTIONS ***/
+const socket = io.connect("http://192.168.1.124:8000");
+const isRoomModalOpen = ref(true)
+const roomCode = ref("")
+const roomCodeError = ref(false)
+const isRoomFull = ref(false)
+const playerIndex = ref(3)  // 1 = player1, 2 = player2, (>= 3) = room's full
+const waitingOpponent = ref(true)
+const waitingString = ref("Waiting opponent")
+
+const joinRoom = () => {
+  if(roomCode.value.length >= 4)
+    socket.emit("joinRoom", roomCode.value)
+  else
+    roomCodeError.value = true
+}
+socket.on("playerIndex", (_playerIndex) => {
+  playerIndex.value = _playerIndex
+  if(playerIndex.value > 2)
+    isRoomFull.value = true
+  else
+    isRoomModalOpen.value = false
+});
+socket.on("startGame", (firstPlayer) => {
+  waitingOpponent.value = false
+  if(playerIndex.value === firstPlayer)
+    playerColor.value = 'blue'
+  else
+    playerColor.value = 'pink'
+});
+socket.on("move", (args) => {
+  userSelection.value = args.userSelection
+  selectBoardTile(args.payload)
+})
+/*** ### ### ### ***/
 
 const BOARD_DIM = 4
   const props = defineProps({
@@ -19,8 +60,6 @@ const turn = ref(0)
 const canMove = ref(true)
 const points = ref([0,0])
 const gameOver = ref(false)
-
-const isRoomModalOpen = ref(true)
 
 
 /*
@@ -63,6 +102,15 @@ const selectBoardTile = (payload) => {
     else {
       pinkTiles.value[userSelection.value.position].played = true
     }
+
+    // Emit mmove to online opponent
+    if(isOnlineGame && userSelection.value.color === playerColor.value) 
+      socket.emit("play", { 
+        roomCode: roomCode.value,
+        payload: payload,
+        userSelection: userSelection.value,
+      })
+    
     checkGameOver()
     turn.value += 1
     setBoardTilesPlayable(payload)
@@ -265,6 +313,9 @@ const clearSelection = () => {
 }
 // Allow current turn user to select only one own tile piece
 const selectTile = (payload) => {
+  // When online game don't let select opponent's tiles
+  if( (isOnlineGame && waitingOpponent.value) || (isOnlineGame && payload.color !== playerColor.value) )
+    return
   if(props.ai && ((turn.value % 2 === 0 && playerColor.value === 'pink') || (turn.value % 2 !== 0 && playerColor.value === 'blue'))) return
   if(payload.color === 'blue' && turn.value % 2 === 0) {
     if(blueTiles.value[payload.position].selected) {
@@ -293,6 +344,11 @@ watchEffect(() => {
     setAllBoardTilesPlayable(false)
     calculateWinner()
   }
+
+  if(roomCode.value) {
+    roomCodeError.value = false
+    isRoomFull.value = false
+  }
 })
 
 
@@ -317,7 +373,8 @@ const aiPlay = () => {
 ### RESET CONFIG & INITIALIZE GAME ###
 */
 const resetGame = () => {
-  playerColor.value = props.playerColor === null ? null : props.playerColor === 'blue' ? 'blue' : props.playerColor === 'pink' ? 'pink' : Math.round(Math.random()) === 0 ? 'blue' : 'pink'
+  if(!isOnlineGame)
+    playerColor.value = props.playerColor === null ? null : props.playerColor === 'blue' ? 'blue' : props.playerColor === 'pink' ? 'pink' : Math.round(Math.random()) === 0 ? 'blue' : 'pink'
   userSelection.value = null
   turn.value = 0
   canMove.value = true
@@ -348,28 +405,28 @@ resetGame()
         <!-- MODAL -->
         <Teleport to="#modal">
           <Transition name="modal">
-            <div v-if="isRoomModalOpen" class="fixed w-screen h-screen top-0 left-0 bg-black/30 flex justify-center items-center">
-              <div ref="soloModal" class="relative bg-gradient-to-b from-blue-50 to-pink-50 px-16 py-10 rounded-md drop-shadow-md">
+            <div v-if="isRoomModalOpen" class="fixed w-screen h-screen top-0 left-0 bg-black/30 flex justify-center items-center font-fam">
+              <div ref="soloModal" class="relative bg-gradient-to-b from-blue-50 to-pink-50 px-12 py-10 rounded-md drop-shadow-md">
 
-                <input 
+                <h6 class="uppercase text-base w-full flex justify-center items-center text-center font-bold mb-2">create or join a game</h6>
+                <input
+                  v-model="roomCode"
                   placeholder="room code: 1234"
-                  class="flex py-1 w-full justify-center items-center rounded-md uppercase font-fam bg-white border border-blue-900/20 betterhover:hover:border-blue-900/50
-                   focus:border-blue-900/50 outline-none duration-300 drop-shadow-sm text-center text-base"
+                  :class="`flex py-1 w-full justify-center items-center rounded-md uppercase bg-white border border-blue-900/20 betterhover:hover:border-blue-900/50
+                   focus:border-blue-900/50 outline-none duration-300 drop-shadow-sm text-center text-base ${roomCodeError ? 'border-red-500/80' : ''}`"
                 />
+                <p v-show="roomCodeError" class="text-sm text-red-500/80 flex-wrap text-center mt-1">minimum 4 characters</p>
+                <p v-show="isRoomFull" class="text-sm text-red-500/80 flex-wrap text-center mt-1">this room is full</p>
 
                 <button 
-                  class="flex mt-6 px-2 py-1 w-full justify-center items-center rounded-md uppercase font-fam text-xl bg-blue-900/10 betterhover:hover:bg-blue-900/20 duration-300 drop-shadow-sm"
-                >
-                  Create game
-                </button>
-                <button 
-                  class="flex mt-3 py-1 w-full justify-center items-center rounded-md uppercase font-fam text-xl bg-blue-900/10 betterhover:hover:bg-blue-900/20 duration-300 drop-shadow-sm"
+                  @click="joinRoom"
+                  class="flex mt-3 py-1 w-full justify-center items-center rounded-md uppercase text-xl bg-blue-900/10 betterhover:hover:bg-blue-900/20 duration-300 drop-shadow-sm"
                 >
                   Join game
                 </button>
                 <router-link 
                   :to="`/`"
-                  class="flex mt-3 py-1 w-full justify-center items-center rounded-md uppercase font-fam text-xl bg-blue-900/10 betterhover:hover:bg-blue-900/20 duration-300 drop-shadow-sm"
+                  class="flex mt-6 py-1 w-full justify-center items-center rounded-md uppercase text-xl bg-blue-900/10 betterhover:hover:bg-blue-900/20 duration-300 drop-shadow-sm"
                 >
                   Back home
                 </router-link>
@@ -407,7 +464,7 @@ resetGame()
             {{ turn % 2 === 0 ? 'Pink ' : 'Blue '}}<span class="text-black">can't move</span>
           </h6>
           <h6 v-else :class="`text-xs lg:text-sm font uppercase text-black ${gameOver ? 'opacity-0' : 'opacity-100'}`">
-            {{ props.ai ? ((turn % 2 === 0 && playerColor === 'blue') || (turn % 2 !== 0 && playerColor === 'pink')) ? 'Make a move' : "Opponent's turn" : 'Make a move' }}
+            {{ waitingOpponent ? waitingString : props.ai || isOnlineGame ? ((turn % 2 === 0 && playerColor === 'blue') || (turn % 2 !== 0 && playerColor === 'pink')) ? 'Make a move' : "Opponent's turn" : 'Make a move' }}
           </h6>
         </div>
         <!-- BOARD -->
